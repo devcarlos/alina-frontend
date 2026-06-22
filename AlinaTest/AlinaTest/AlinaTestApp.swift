cf77a68ce71ef6155a6b95ecc409b8efcc58c30a
Carlos Alcala <carlos.alcala@me.com>
2026-06-22 15:40:24 -0400

    Base Project

---
+++ AlinaTest/AlinaTest/AlinaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlinaTestApp.swift
+//  AlinaTest
+//
+//  Created by Carlos Alcala on 22/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AlinaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
